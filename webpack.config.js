49d18920f94c51f369ea35609f7ffdd00206e178
Max Wilson <wilson.max@gmail.com>
2020-05-22 13:57:57 -0700

    Having some troubles with plotly

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 module.exports = {
     mode: "none",
-    entry: "./src/App.fsproj",
+    entry: "./src/Saves.fsproj",
     devServer: {
         contentBase: path.join(__dirname, "./dist")
     },
