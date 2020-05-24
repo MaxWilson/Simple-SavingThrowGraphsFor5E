e83f6e2e3e5cb9642cdc7bb93e84b91e55e80346
Max Wilson <wilson.max@gmail.com>
2020-05-24 13:33:58 -0700

    Start adding encounter construction

---
+++ src/Compute.fs
@@ -0,0 +1,66 @@
+﻿module Compute
+
+type Stats = {
+    str: int * int option
+    dex: int * int option
+    con: int * int option
+    int: int * int option
+    wis: int * int option
+    cha: int * int option
+    advantage: string
+    ac: int
+    hp: int
+    speed: string
+    magicResistance: bool
+    dcStr: int option
+    dcDex: int option
+    dcCon: int option
+    dcInt: int option
+    dcWis: int option
+    dcCha: int option
+    skills: (string * int) list
+    damageResistances: string
+    damageImmunities: string
+    damageVuln: string
+    conditionImmunities: string list
+    }
+type Header = {
+    name: string
+    stats: Stats
+    cr: float
+    size: string
+    creatureType: string
+    tags: string list
+    alignment: string
+    ac: int
+    hp: int
+    legendary: bool
+    unique: bool
+    source: string
+    sourcebook: string
+    }
+
+let mutable creatures: Header list = []
+#if INTERACTIVE
+#r "nuget: Newtonsoft.Json"
+open Newtonsoft.Json
+creatures <- System.IO.File.ReadAllText(sprintf @"c:\code\saves\data.json") |> JsonConvert.DeserializeObject<Header list>
+#endif
+
+let byName = creatures |> Seq.map (fun m -> m.name, m) |> Map.ofSeq
+let sources = [|"Out of the Abyss"; "Princes of the Apocalypse";
+    "Hoard of the Dragon Queen"; "Rise of Tiamat"; "Monster Manual";
+    "Princes of the Apocalypse Online Supplement v1"; "Basic Rules v1";
+    "HotDQ supplement"; "Player's Handbook"; "Mordenkainen's Tome of Foes";
+    "Storm King's Thunder"; "Curse of Strahd"; "Tales from the Yawning Portal";
+    "Tomb of Annihilation"; "The Tortle Package"; "Volo's Guide to Monsters";
+    "Eberron - Rising from the Last War"|]
+let crs = [0.; 0.125; 0.25; 0.5] @ [1. .. 1. .. 30.]
+let bySrcCR = creatures |> Seq.groupBy (fun m -> m.sourcebook, m.cr) |> Map.ofSeq
+
+for cr in crs do
+    match bySrcCR |> Map.tryFind ("Tomb of Annihilation", cr) with
+    | Some monsters ->
+        for m in monsters do
+            printfn "%.2g %s" cr m.name
+    | None -> ()
---
+++ src/Main.fs
@@ -0,0 +1,141 @@
+module App
+
+open Elmish
+open Elmish.React
+open Feliz
+
+open Feliz.Plotly
+
+let chart (lines: Map<string, float list>) =
+    let colors = [(0, 100, 80); (0, 176, 246); (231, 107, 243)]
+    let mkColor ix = colors.[ix % colors.Length] |> color.rgb
+    let mkFill ix = colors.[ix % colors.Length] |> (fun (x,y,z) -> color.rgba(x,y,z,0.2))
+    let hover key ix =
+        let data = lines.[key]
+        traces.scatter [
+            scatter.x ([ 1 .. (data.Length) ] |> List.map float)
+            scatter.y data
+            scatter.marker [
+                marker.color (colors.[ix % colors.Length] |> color.rgb)
+                ]
+            scatter.text ([ 1 .. (data.Length) ] |> List.map (fun i -> (sprintf "%s(%.1f, %.1f)" key (float i) data.[i-1])))
+            scatter.hoverinfo.text
+            scatter.showlegend false
+            scatter.hoveron.points
+            scatter.mode.markers
+            ]
+    let myline key ix =
+        let data = lines.[key]
+        traces.scatter [
+            scatter.x ([ 1 .. (data.Length) ] |> List.map float)
+            scatter.y data
+            scatter.line [
+                line.color (colors.[ix % colors.Length] |> color.rgb)
+            ]
+            scatter.mode.lines
+            scatter.name key
+            scatter.hoverinfo.skip
+            ]
+    let background key ix =
+        let data = lines.[key]
+        let xs = ([ 1 .. (data.Length) ] @ [ (data.Length) .. -1 .. 1 ]) |> List.map float
+        traces.scatter [
+            scatter.x xs
+            scatter.y ((data |> List.map ((+) 2.))@(data |> List.rev |> List.map (fun x -> x - 2.)))
+            scatter.fill.toself
+            scatter.fillcolor (colors.[ix % colors.Length] |> (fun (x,y,z) -> color.rgba(x,y,z,0.2)))
+            scatter.line [
+                line.color color.transparent
+            ]
+            scatter.name key
+            scatter.showlegend false
+            scatter.hoverinfo.skip
+        ]
+    Plotly.plot [
+        plot.traces [
+            for name, ix in lines |> Seq.mapi(fun i (KeyValue(name,_)) -> name, i) do
+                myline name ix
+                background name ix
+                hover name ix
+        ]
+        plot.layout [
+            layout.paperBgcolor (color.rgb(255, 255, 255))
+            layout.plotBgcolor (color.rgb(229, 229, 229))
+            layout.xaxis [
+                xaxis.gridcolor (color.rgb(255, 255, 255))
+                xaxis.range [ 1; 10 ]
+                xaxis.showgrid true
+                xaxis.showline false
+                xaxis.showticklabels true
+                xaxis.tickcolor (color.rgb(127, 127, 127))
+                xaxis.ticks.outside
+                xaxis.zeroline false
+            ]
+            layout.yaxis [
+                yaxis.gridcolor (color.rgb(255, 255, 255))
+                yaxis.showgrid true
+                yaxis.showline false
+                yaxis.showticklabels true
+                yaxis.tickcolor (color.rgb(127, 127, 127))
+                yaxis.ticks.outside
+                yaxis.zeroline false
+            ]
+            layout.hovermode.closest
+        ]
+    ]
+
+type State =
+    { Count: int; lines: Map<string, float list> }
+
+type Msg =
+    | Increment
+    | Decrement
+
+let lines =
+    [
+    "Fair", [1..10] |> List.map float
+    "Premium", [ 5.; 2.5; 5.; 7.5; 5.; 2.5; 7.5; 4.5; 5.5; 5. ]
+    "Ideal", [ 10; 8; 6; 4; 2; 0; 2; 4; 2; 0 ] |> List.map float
+    ] |> Map.ofList
+
+let r = System.Random()
+let setLength (s: State) =
+    let l = s.Count
+    let adjust _ (line: float list) =
+        if line.Length = l then line
+        elif line.Length > l then List.take l line
+        else line @ [r.NextDouble() * 10.]
+    { s with lines = s.lines |> Map.map adjust }
+
+
+let init() =
+    { Count = lines |> Seq.map(fun (KeyValue(_, points)) -> points.Length) |> Seq.max; lines = lines }
+
+let update (msg: Msg) (state: State): State =
+    match msg with
+    | Increment ->
+        { state with Count = state.Count + 1 } |> setLength
+
+    | Decrement ->
+        { state with Count = state.Count - 1 } |> setLength
+
+
+let render (state: State) (dispatch: Msg -> unit) =
+  Html.div [
+    Html.button [
+      prop.onClick (fun _ -> dispatch Increment)
+      prop.text "Increment!"
+    ]
+
+    Html.button [
+      prop.onClick (fun _ -> dispatch Decrement)
+      prop.text "Decrement"
+    ]
+
+    Html.h1 state.Count
+    chart state.lines
+  ]
+
+Program.mkSimple init update render
+|> Program.withReactSynchronous "elmish-app"
+|> Program.run
