8e1e46acd12a8810e9c9aa3ade802bf69cddcf62
Max Wilson <wilson.max@gmail.com>
2020-06-07 11:40:36 -0700

    Towards refactor

--- src/Model.fs
+++ src/Model.fs
@@ -26,6 +26,54 @@ module Wizard =
         | TypeFilter of string list
         | ScaleByLegendaryResist of bool
         | XanatharStyle of Compute.XanatharMethod
+module Construct =
+    type Analysis = | PureCR | Encounter
+    type EncounterBuilder = | Xanathar | DMG | ShiningSword
+    type DC = Fixed of int option | Dynamic
+    type Targeting = Single | AoE of int * float
+    type Settings = {
+        analysis: Analysis option
+        encounterBuilder: EncounterBuilder option
+        difficulty: Difficulty option
+        partySize: int option
+        sources: string list
+        types: string list
+        xanatharStyle: Compute.XanatharMethod option
+        }
+    let (|Analysis|_|) settings = settings.analysis
+    let (|EncounterBuilder|_|) settings = settings.encounterBuilder
+    let (|Difficulty|_|) settings = settings.difficulty
+    let (|XanatharStyle|_|) settings = settings.xanatharStyle
+    let (|PartySize|_|) settings = settings.partySize
+    let analysisChoices = [PureCR, "Monsters (by CR)"; Encounter, "Encounters (by PC level)"]
+    let (|ConstructionMethod|_|) = function
+        | Analysis PureCR -> Some Compute.PureCR
+        | Analysis Encounter & EncounterBuilder(Xanathar) & Difficulty(Easy | Medium | Hard as diff) & XanatharStyle style ->
+            Compute.Xanathar(style, diff) |> Some
+        | Analysis Encounter & EncounterBuilder(DMG) & Difficulty(diff) ->
+            Compute.DMG diff |> Some
+        | Analysis Encounter & EncounterBuilder(ShiningSword) & Difficulty(diff) ->
+            Compute.ShiningSword diff |> Some
+        | _ -> None
+    let (|ConstructionSettings|_|) settings =
+        let settingsFor partySize method =
+            {
+            ConstructionSettings.partySize = partySize
+            sources = settings.sources
+            creatureType = settings.types
+            method = method
+            } |> Some
+        match settings with
+        | Analysis PureCR ->
+            settingsFor 1 Compute.PureCR
+        | Analysis Encounter & PartySize n & EncounterBuilder(Xanathar) & Difficulty(Easy | Medium | Hard as diff) & XanatharStyle style ->
+            settingsFor n (Compute.Xanathar(style, diff))
+        | Analysis Encounter & PartySize n& EncounterBuilder(DMG) & Difficulty(diff) ->
+            settingsFor n (Compute.DMG diff)
+        | Analysis Encounter & PartySize n & EncounterBuilder(ShiningSword) & Difficulty(diff) ->
+            settingsFor n (Compute.ShiningSword diff)
+        | _ -> None
+
 type Model = {
     choices: Wizard.Choice list
     constructSettings: ConstructionSettings
